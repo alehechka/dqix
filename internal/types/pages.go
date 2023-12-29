9f65edfb38154c38d8ed589b99f21c9ebb49bb17
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2023-12-28 21:44:09 -0600

    remaining sidenav links

--- internal/types/pages.go
+++ internal/types/pages.go
@@ -1,8 +1,69 @@
 package types
 
 const (
-	ClassEverydayItem  string = "everyday-item"
-	ClassImportantItem string = "important-item"
-	ClassHeadArmor     string = "head"
-	ClassTorsoArmor    string = "torso"
+	ClassItemEveryday  string = "everyday-item"
+	ClassItemImportant string = "important-item"
+
+	ClassArmorHead   string = "head"
+	ClassArmorTorso  string = "torso"
+	ClassArmorShield string = "shield"
+	ClassArmorArms   string = "arms"
+	ClassArmorLegs   string = "legs"
+	ClassArmorFeet   string = "feet"
+
+	ClassWeaponAxe       string = "axe"
+	ClassWeaponBoomerang string = "boomerang"
+	ClassWeaponBow       string = "bow"
+	ClassWeaponClaw      string = "claw"
+	ClassWeaponFan       string = "fan"
+	ClassWeaponHammer    string = "hammer"
+	ClassWeaponKnife     string = "knife"
+	ClassWeaponSpear     string = "spear"
+	ClassWeaponStave     string = "stave"
+	ClassWeaponSword     string = "sword"
+	ClassWeaponWand      string = "wand"
+	ClassWeaponWhip      string = "whip"
 )
+
+func IsWeapon(class string) bool {
+	switch class {
+	case ClassWeaponAxe,
+		ClassWeaponBoomerang,
+		ClassWeaponBow,
+		ClassWeaponClaw,
+		ClassWeaponFan,
+		ClassWeaponHammer,
+		ClassWeaponKnife,
+		ClassWeaponSpear,
+		ClassWeaponStave,
+		ClassWeaponSword,
+		ClassWeaponWand,
+		ClassWeaponWhip:
+		return true
+	default:
+		return false
+	}
+}
+
+func IsArmor(class string) bool {
+	switch class {
+	case ClassArmorHead,
+		ClassArmorTorso,
+		ClassArmorShield,
+		ClassArmorArms,
+		ClassArmorLegs,
+		ClassArmorFeet:
+		return true
+	default:
+		return false
+	}
+}
+
+func IsItem(class string) bool {
+	switch class {
+	case ClassItemEveryday, ClassItemImportant:
+		return true
+	default:
+		return false
+	}
+}
