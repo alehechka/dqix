c1dd688ce851f8a9eee42d97e4a42fa8b5e3496e
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2024-01-06 16:35:29 -0600

    change theme toggle to client-side only

--- pkg/gin/theme.go
+++ pkg/gin/theme.go
@@ -1,30 +1,17 @@
 package gin_utils
 
 import (
-	"dqix/web/templ/components/buttons"
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 const (
-	CookieTheme string = "theme"
-	ThemeDark   string = "dark"
-	ThemeLight  string = "light"
+	AttributeDataMode string = "data-mode"
+	CookieTheme       string = "theme"
+	ThemeDark         string = "dark"
+	ThemeLight        string = "light"
 )
 
 func IsDarkMode(ctx *gin.Context) bool {
 	theme, err := ctx.Cookie(CookieTheme)
 	return err == nil && theme == ThemeDark
-}
-
-func ToggleThemeHandler(ctx *gin.Context) {
-	isDarkMode := IsDarkMode(ctx)
-
-	value := ThemeDark
-	if isDarkMode {
-		value = ThemeLight
-	}
-	ctx.SetCookie(CookieTheme, value, 0, "", "", false, true)
-	ctx.HTML(http.StatusOK, "", buttons.ThemeToggleButton(!isDarkMode))
 }
