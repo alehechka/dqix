5dfb27aa62d27937055b562d3d4869c8f1c89d08
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2024-01-17 10:11:59 -0600

    reverse logic in howToMake to avoid decrementing

--- internal/types/inventory.go
+++ internal/types/inventory.go
@@ -396,13 +396,13 @@ func (p PageContent) parseFromBase(inventory *Inventory) {
 				inventory.Recipe = make(map[string]int)
 			}
 			for i++; i < lastIndex; i += 2 {
-				if !strings.HasPrefix(p.Text[i+1], "x") {
-					i--
-					break
-				}
 				id := TitleToID(p.Text[i])
 				num, _ := strconv.Atoi(strings.Split(strings.TrimPrefix(p.Text[i+1], "x"), " ")[0])
 				inventory.Recipe[id] = num
+
+				if i+3 >= lastIndex || !strings.HasPrefix(p.Text[i+3], "x") {
+					break
+				}
 			}
 		case "Where to find:":
 			for i++; i < lastIndex; i++ {
