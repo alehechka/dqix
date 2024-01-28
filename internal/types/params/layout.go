dc7df56519dd61fcd7ec6ca858ff92deab6866e2
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2024-01-27 21:59:33 -0600

    add GetIconPath method to params.Layout

--- internal/types/params/layout.go
+++ internal/types/params/layout.go
@@ -5,6 +5,16 @@ type Layout struct {
 	Page       string
 	IsDarkMode bool
 	CSSVersion string
+	IconPath   string
+}
+
+// GetIconPath returns the IconPath when it not empty, otherwise will return the default path to the app's favicon.
+func (l Layout) GetIconPath() string {
+	if l.IconPath != "" {
+		return l.IconPath
+	}
+
+	return "/static/favicon.ico"
 }
 
 type Index struct {
